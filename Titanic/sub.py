5fca0a18dc113c1e2469b7a4b2c5dff683f9474a
Connal de Souza <connaldesouza96@gmail.com>
2018-06-11 01:46:08 -0700

    Added switch in optimizer and batch size near the end of training

--- Titanic/sub.py
+++ Titanic/sub.py
@@ -52,7 +52,7 @@ for i in range(len(data.columns)):
 dmat = np.concatenate(cols, 0).T#.reshape([len(cols), len(cols[0])])
 tdmat = np.concatenate(tcols, 0).T#.reshape([len(cols), len(cols[0])])
 
-NHID = 2
+NHID = 5
 VALIDATION_SIZE = 50
 v_dmat = dmat[:VALIDATION_SIZE]
 dmat = dmat[VALIDATION_SIZE:]
@@ -72,24 +72,33 @@ y = tf.matmul(layer1, W1) + b1
 y_ = tf.placeholder(tf.float32, [None, 1])
 
 cross_entropy = tf.losses.sigmoid_cross_entropy(multi_class_labels=y_, logits=y)
-train_step = tf.train.AdadeltaOptimizer(2).minimize(cross_entropy)
+train_step = tf.train.AdadeltaOptimizer(5).minimize(cross_entropy)
 sess = tf.Session()
 tf.global_variables_initializer().run(session=sess)
 prediction = tf.round(tf.sigmoid(y))
 # print(W.eval(session = sess))
 # Test trained model
+batch_size = 300
+end_batch_size = 800
+switched = False
 correct_prediction = tf.equal([tf.round(tf.sigmoid(y))], [y_])
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-for i in range(30000):
-    batch_xs, batch_ys = get_batch(dmat, answers,300)
-    # sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    sess.run(train_step, feed_dict={x: dmat, y_: answers.reshape(len(answers), 1)})
+for i in range(20000):
+    batch_xs, batch_ys = get_batch(dmat, answers,batch_size)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    # sess.run(train_step, feed_dict={x: dmat, y_: answers.reshape(len(answers), 1)})
     if i % 300 == 0:
         print('iteration:', i)
         # print(W0.eval(session = sess))
         print('Train:', accuracy.eval(session = sess, feed_dict={x: batch_xs, y_: batch_ys}))
         batch_xs, batch_ys = get_batch(v_dmat,v_answers,VALIDATION_SIZE)
-        print('Validation:', accuracy.eval(session = sess, feed_dict={x: batch_xs, y_: batch_ys}))
+        v_acc = accuracy.eval(session = sess, feed_dict={x: batch_xs, y_: batch_ys})
+        print('Validation:', v_acc)
+        if not switched and v_acc > 0.8:
+            print('SWITCH')
+            train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+            batch_size = end_batch_size
+            switched = True
         # print(cross_entropy.eval(session = sess, feed_dict={x: batch_xs, y_: batch_ys}))
 
 batch_xs, batch_ys = get_batch(v_dmat, v_answers, VALIDATION_SIZE)
